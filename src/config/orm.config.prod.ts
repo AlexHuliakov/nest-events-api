770696bae5e0b33381ea8105b6c1eeb4c145f235
AlexRazor <alexrazor1337@gmail.com>
2023-08-29 17:40:03 +0300

    Update orm config

--- src/config/orm.config.prod.ts
+++ src/config/orm.config.prod.ts
@@ -3,24 +3,26 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export default registerAs(
   'orm.config',
-  (): TypeOrmModuleOptions => (
-    if (process.env.DATABASE_URL) {
-      return {
-        type: 'postgres',
-        url: process.env.DATABASE_URL,
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        synchronize: false,
+  (): TypeOrmModuleOptions => 
+    {
+      if (process.env.DATABASE_URL) {
+        return {
+          type: 'postgres',
+          url: process.env.DATABASE_URL,
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: false,
+        }
+      } else {
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: false,
+        }  
       }
-    } else {
-      return {
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        synchronize: false,
-      }  
-    }),
+    },
 );
